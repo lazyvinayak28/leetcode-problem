8276807e33a6313676f94483c4027e7e58b88c9f
Vinayak Mishra <vinayakmishra2805@gmail.com>
2026-07-31 17:48:12 +0530

    Time: 218 ms (8.92%), Space: 147.6 MB (5.12%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    void generate_combinations(vector<int> &t , vector<int> nums,vector<vector<int>> &ans,int &k){
-        if(t.size()==k){ans.push_back(t);return ;}
-        if(t.size()>k){return;}
-        if(nums.size()==0){return;}
-        t.push_back(nums[0]);
-        vector<int> temp(nums.begin()+1,nums.end());
-        generate_combinations(t,temp,ans,k);
-        t.pop_back();
-        generate_combinations(t,temp,ans,k);
+    void gen_combo(vector<int> &t , vector<int> nums,int idx,vector<vector<int>> &ans, int &k){
+        if(t.size()==k){ans.push_back(t);return;}
+        if(t.size()>k){return ;}
+        for(int i=idx;i<nums.size();i++){
+            t.push_back(nums[i]);
+            gen_combo(t,nums,i+1,ans,k);
+            t.pop_back();
 
+        }
     }
     vector<vector<int>> combine(int n, int k) {
         vector<int>  nums;
-        vector<int> t;
         vector<vector<int>> ans;
+        vector<int> t;
+        int idx=0;
         for(int i=0;i<n;i++){
             nums.push_back(i+1);
         }
-        generate_combinations(t,nums,ans,k);
+        gen_combo(t,nums,idx,ans,k);
         return ans;
+        
     }
 };
