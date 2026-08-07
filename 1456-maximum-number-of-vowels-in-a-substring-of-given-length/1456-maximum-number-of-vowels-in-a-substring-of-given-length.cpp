c2d0f65e14d7d4716d3720e86e967fdb0f67f08d
Vinayak Mishra <vinayakmishra2805@gmail.com>
2026-08-07 18:59:25 +0530

    Time: 3 ms (88.25%), Space: 16.8 MB (6.97%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,13 +3,15 @@ public:
     int maxVowels(string s, int k) {
         int n=s.size();
         vector<int> freq(n,0);
-        for(int p=0;p<n;p++){
+        int p=0;
+        while(p<n){
             if(s[p]=='a' || s[p]=='e' || s[p]=='i' || s[p]=='o' || s[p]=='u'){
                 freq[p]=1;
             }
+            p++;
         }
-        int i=0,j=k-1;int csum=0,msum=0;
-        for(int p=i;p<=j;p++){csum+=freq[p];}
+        int i=0,j=k-1;int csum=0,msum=0;p=i;
+        while(p<=j){csum+=freq[p];p++;}
         msum=csum;
         while(j<n-1){
             csum=csum-freq[i]+freq[j+1];
