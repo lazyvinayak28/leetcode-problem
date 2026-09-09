d70d7fb1baf0464a28de7eb56c637414a2c52f79
Vinayak Mishra <vinayakmishra2805@gmail.com>
2026-09-09 23:52:59 +0530

    Time: 0 ms (100%), Space: 67.2 MB (55.43%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
+        //learning ternary in the way 
+        //remember this is just qan approach based on rememberance and not the one main
         return accumulate(arr.begin() + 1, arr.end(), pair{arr[0], 0}, [k](auto p, int x) { return p.second == k ? p : (p.first > x ? pair{p.first, p.second + 1} : pair{x, 1}); }).first;
     }
 };
